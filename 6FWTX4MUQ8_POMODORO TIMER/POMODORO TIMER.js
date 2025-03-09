6999118899290d22da231ac81f5c550904bd4811
PrasadManchikalapati <139560567+PrasadManchikalapati@users.noreply.github.com>
2025-03-09 13:33:26 +0530

    Add files via upload

---
+++ 6FWTX4MUQ8_POMODORO TIMER/POMODORO TIMER.js
@@ -0,0 +1,17 @@
+let time = document.getElementById('time')
+
+function reset() {
+    time.textContent = "25:00";
+}
+
+function short() {
+    time.textContent = "05:00";
+}
+
+function long() {
+    time.textContent = "15:00";
+}
+
+function start() {
+    let sample
+}
